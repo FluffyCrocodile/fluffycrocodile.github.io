e36f43da6296586dc1a98a61e3ee54017184d188
FluffyCrocodile <68883661+FluffyCrocodile@users.noreply.github.com>
2021-04-10 17:22:13 -0700

    k

--- README.md
+++
@@ -1,5 +0,0 @@
-# CS290-Server-Side-Examples
-
-./node_modules/forever/bin/forever stasrt main.js portnumber
-
-
--- public/filterfaculty.js
+++
@@ -1,7 +0,0 @@
-function filterFacultyByID() {
-    //get the id of the selected homeworld from the filter dropdown
-    var homeworld_id = document.getElementById('homeworld_filter').value
-    console.log(homeworld_id);
-    //construct the URL and redirect to it
-    window.location = '/faculty/filter/' + parseInt(homeworld_id)
-}
